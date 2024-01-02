1481cb8f02909b35dbe0962f6d8189b491c831c9
moses <moses.mwangi.me@gmail.com>
2024-01-02 09:33:41 +0300

    commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strick";
 
 ///////////////////////// modal game ///////////////////////////////
-/*
+
 // document.querySelector(".modal").classList.remove("hidden");
 // document.querySelector(".overlay").classList.remove("hidden");
 const modalEl = document.querySelector(".modal");
@@ -38,4 +38,110 @@ document.addEventListener("keydown", function (e) {
     document.querySelector(".modal").classList.add("hidden");
     document.querySelector(".overlay").classList.add("hidden");
   }
-});*/
+});
+
+// /const creatBooking = function (flight, plane = 1, air = 5) {
+//   // plane ||= 1;
+//   // air ||= 5;
+//   const Booking = { flight, plane, air };
+//   console.log(Booking);
+// };
+// creatBooking("LB23");
+
+// const flight = "LB999";
+// const moses = { name: "moses mwangi", passport: 23575398932 };
+
+// const check = function (flightNun, passager) {
+//   flightNun = "LG90";
+//   passager.name = "MR" + " " + passager.name;
+//   if (passager.passport === 23575398932) {
+//     alert("check In");
+//   } else {
+//     alert("wrong passport");
+//   }
+// };
+// check(flight, moses);
+// console.log(flight);
+// console.log(moses);
+
+// const newPass = function (person) {
+//   person.passport = Math.trunc(Math.random() * 100000000000000);
+// };
+// newPass(moses);
+// check(flight, moses);
+
+/////////// function accepting call back///////////
+/*
+const oneWord = function (str) {
+  return str.replaceAll(" ", "").toLowerCase();
+};
+
+const upperWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), others].join(" ");
+};
+
+const word = function (str) {
+  const split = str.split(" ");
+
+  return split;
+};
+//////////// high order function ////////////////////
+
+const transformer = function (str, fn) {
+  console.log(`original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed : ${fn.name}`);
+};
+transformer("Javascript is just awesome", upperWord);
+transformer("Javascript is just awesome ????", oneWord);
+transformer("Javascript is just awesome ////////////", word);
+
+/////////////////////////returning function////////////
+
+//const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+//};
+
+// const greet = (greeting) =>
+//   function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+
+const greet = (greeting) => (name) => console.log(`${greeting} ${name}`);
+
+const greetHey = greet("Hey");
+greetHey("mwangi");
+greet("Hello")("moses");*/
+
+const luft = {
+  Airline: "kenya airways",
+  iatCode: "LHB",
+  booking: [],
+  book(flightNun, name) {
+    console.log(
+      `${name} book a seat on ${this.Airline} flight ${this.iatCode}${flightNun}`
+    );
+    this.booking.push({ flight: `${this.iatCode}${flightNun}`, name });
+  },
+};
+
+console.log(luft);
+luft.book(23, "moses mwangi");
+luft.book(23, "mwangi");
+
+const eurowing = {
+  name: "Eurowings",
+  iatCode: "EW",
+  booking: [],
+};
+
+const book = luft.book;
+// console.log(book);
+// book(23, "sarah williams");
+book.call(eurowing, 23, "sarah williams");
+console.log(eurowing);
+book.call(luft, 900, "Wanjiro");
+console.log(luft);
